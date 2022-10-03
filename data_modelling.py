d503e78abb72aaa92123cfd71fc966bf77d98228
dit-x <daranijotimothy@gmail.com>
2022-10-03 09:05:12 +0100

    add: modeling data script

--- data_modelling.py
+++ data_modelling.py
@@ -23,7 +23,8 @@ def load_data(path="ycombinator.csv"):
     df['founded'] = df['founded'].fillna('-1').astype(int)
     df['country'] = df.full_location.apply(lambda x: x.split(',')[-1].strip())
 
-    company_df = df[['company_id', 'company_name', 'link', 'short_description', 'founded', 'team_size', 'location', 'country', 'no_founders', 'description']]
+    company_df = df[['company_id', 'company_name', 'link', 'short_description', 'founded', 'team_size', 'location', 'country', 'no_founders', 'no_company_socials', 'no_tags','description']]
+    company_df.columns = ['id', 'company_name', 'link', 'short_description', 'founded', 'team_size', 'location', 'country', 'no_founders', 'no_company_socials', 'no_tags', 'description']
 
     return df, company_df
 
@@ -36,13 +37,14 @@ def founder_data(df):
     founders = pd.concat([df['company_id'], founders], axis=1)
     founders = founders[~founders['name'].isna()]
     founders.insert(0, 'founder_id', range(1, 1 + len(founders)))
-    founder_table = founders[['founder_id', 'name', 'role', 'company_id']]
 
-    return founder_table
+    return founders
 
 
-def founder_social(founder_df, socials_mapper):
-    f_social = founder_df.explode("social_media_links")
+def founder_and_social(df, socials_mapper):
+    founders_info = founder_data(df)
+    founder_info_only = founders_info[['founder_id', 'name', 'role', 'company_id']]
+    f_social = founders_info.explode("social_media_links")
     f_social['social_media_links'] = f_social['social_media_links'].astype(str)
     f_social['social_media_links'] = f_social['social_media_links'].str.lower().str.replace(r'(//[a-zA-Z]{2}[.])', '//')
     f_social['social_name'] = f_social['social_media_links'].str.extract(r'(https://)(www.|mobile.)?([a-zA-Z0-9]*)')[2]
@@ -53,8 +55,9 @@ def founder_social(founder_df, socials_mapper):
     f_social['social_media_id'] = f_social['social_media_id'].astype(int)
     f_social.insert(0, 'id', range(1, 1 + len(f_social)))
 
-    founder_social_to_social = f_social[['id', 'founder_id', 'social_media_id']]
-    return founder_social_to_social
+    founder_social_to_social = f_social[['id', 'social_media_id', 'founder_id']]
+    founder_info_only.columns = ['id', 'name', 'role', 'company_id']
+    return founder_info_only, founder_social_to_social
 
 
 
@@ -75,7 +78,7 @@ def get_social_data(df):
     c_social['social_media_id'] = c_social['social_media_id'].fillna(-1).astype(int)
     c_social = c_social[~c_social['social_name'].isna()]
     c_social.insert(0, 'id', range(1, 1 + len(c_social)))
-    company_social_to_social = c_social[['id', 'company_id', 'social_media_id']]
+    company_social_to_social = c_social[['id', 'social_media_id', 'company_id']]
     
     return socials_df, company_social_to_social, socials_mapper
 
@@ -99,19 +102,22 @@ def get_tags_data(df):
 
 
 all_df, company_df = load_data()
-founder_df = founder_data(all_df)
 social_df, company_social_to_social, socials_mapper = get_social_data(all_df)
-tags_df = get_tags_data(all_df)
+founder_df, founder_social_to_social = founder_and_social(all_df, socials_mapper)
+tag_df, company_to_tag = get_tags_data(all_df)
 
 
-# all_df = pd.read_csv('data/ycombinator_all.csv')
-# company_df = pd.read_csv('data/company.csv')
-# founder_df = pd.read_csv('data/founder.csv')
-# social_df = pd.read_csv('data/socials.csv')
-# tags_df = pd.read_csv('data/tags.csv')
+all_df = pd.read_csv('data/ycombinator_all.csv')
+company_df = pd.read_csv('data/company.csv')
+founder_df = pd.read_csv('data/founder.csv')
+social_df = pd.read_csv('data/socials.csv')
+tags_df = pd.read_csv('data/tags.csv')
+company_to_tag_df = pd.read_csv("data/company_to_tag.csv")
+founder_social_to_social_df = pd.read_csv("data/founder_social_to_social.csv")
+company_social_to_social = pd.read_csv("data/company_social_to_social.csv")
 
 
-founder_sum = founder_df.groupby('company_id')['founder_id'].count().sum()
+founder_sum = founder_df.groupby('company_id')['id'].count().sum()
 if founder_sum == all_df['no_founders'].sum():
     print('\nfounder check passed')
     print('Total sum is', founder_sum)
